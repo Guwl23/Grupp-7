24fea44c472a94cf5568de53f8bc6e29d8010d58
Wigglyo <wigothygesson@icloud.com>
2025-04-29 14:27:02 +0200

    Update project.ino

--- project/project.ino
+++ project/project.ino
@@ -13,6 +13,8 @@
 
 
 // Remember to remove these before commiting in GitHub
+String ssid = "";
+String password = "";
 
 // "tft" is the graphics libary, which has functions to draw on the screen
 TFT_eSPI tft = TFT_eSPI();
